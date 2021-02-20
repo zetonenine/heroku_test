3d33adbb677b2e588d49f1b1991b170a80ec9d91
Andrey Zaytsev <66561503+zetonenine@users.noreply.github.com>
2021-02-21 01:51:16 +0300

    Update main.py

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from aiogram import Bot, Dispatcher, executor, types
 from aiogram.contrib.fsm_storage.redis import RedisStorage
 from aiogram.contrib.fsm_storage.memory import MemoryStorage
 from aiogram.contrib.middlewares.logging import LoggingMiddleware
-import redis
+# import redis
 
 
 logging.basicConfig(level=logging.INFO)
@@ -14,8 +14,7 @@ bot = Bot(token='1233194283:AAEltBBA7sqZ7HPo5Gxef5fALXiqhyRwBqk')
 dp = Dispatcher(bot, storage=MemoryStorage())
 dp.middleware.setup(LoggingMiddleware())
 
-r = redis.Redis()
-
+# r = redis.Redis(db=5)
 
 
 @dp.message_handler(state=None, commands='start')
