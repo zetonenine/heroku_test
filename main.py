df94ec385765c2c6224ae574f9d0c1acf8b3d4c8
Andrey Zaytsev <66561503+zetonenine@users.noreply.github.com>
2021-02-24 17:14:48 +0300

    Update main.py

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ logging.basicConfig(level=logging.INFO)
 
 bot = Bot(token='1233194283:AAEltBBA7sqZ7HPo5Gxef5fALXiqhyRwBqk')
 
-dp = Dispatcher(bot, storage=RedisStorage2(host='ec2-108-128-98-183.eu-west-1.compute.amazonaws.com', port=28589, password='p9fc88e56bed344d5f58352463c019a889910e9cf0b122bb4cfd70977c0f49d18'))
+dp = Dispatcher(bot, storage=RedisStorage2(host='ec2-54-86-106-10.compute-1.amazonaws.com', port=24019, password='pdf629fb003d877580494432330091be688b8366c113b0c062d56d7d742932cc1'))
 # dp = Dispatcher(bot, storage=MemoryStorage())
 dp.middleware.setup(LoggingMiddleware())
 
